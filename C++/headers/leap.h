e32d917c0932e4107bae8b321410fc4d76d10b9a
rubnMartnez <projects.overstay918@passinbox.com>
2025-09-21 19:22:29 +0200

    add leap exercise

---
+++ C++/headers/leap.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace leap {
+
+// TODO: add your solution here
+bool is_leap_year(const int& year);
+
+}  // namespace leap
+
---
+++ C++/leap.cpp
@@ -0,0 +1,15 @@
+#include "headers/leap.h"
+
+namespace leap {
+
+// TODO: add your solution here
+bool is_leap_year(const int& year){
+    if (year % 100 == 0){
+        return year % 400 == 0 ? true : false;
+    } else {
+        return year % 4 == 0;
+    }
+}
+
+}  // namespace leap
+
