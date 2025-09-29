5594b3ce493b03280b11b5025f55832c7b46e425
rubnMartnez <projects.overstay918@passinbox.com>
2025-09-29 11:13:25 +0200

    bob refactor

--- C++/bob.cpp
+++ C++/bob.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cctype>
+#include <iostream>
 
 namespace bob {
 
@@ -11,23 +12,51 @@ std::string hey(const std::string& str){
     result.reserve(32); // preallocate the maximum capacity that a response could take
     result = "Whatever."; // assign default response
 
-    auto firstValidChar = str.find_first_not_of(" ");
-    if (str.empty() || firstValidChar == std::string::npos){
-        return "Fine. Be that way!";
-    }
-    
-    bool hasQuestionMark = str.at(str.size() - 1) == '?';
-    bool isAllUpperCase = std::all_of(str.begin(), str.end(), [](const char& c){
-        return std::isupper(c);
-    });
+    if (str.empty()) return "Fine. Be that way!";
 
-    if (hasQuestionMark){
+    bool isQuestion = false;
+    bool isAllUpperCase = true;
+    bool hasAnyChar = false;
+    bool hasAnyLetters = false;
+    bool firstCharFoundAtEnd = false;
+
+    for (int i = 0, iOpposite = str.size() - 1; i < iOpposite; i++, iOpposite--){
+        if (!std::isspace(str[i])){
+            hasAnyChar = true;
+            if (!std::iscntrl(str[i]) && !std::isdigit(str[i]) && !std::ispunct(str[i])){
+                hasAnyLetters = true;
+                if (!std::isupper(str[i]))
+                    isAllUpperCase = false;
+            }
+        }
+        if (!std::isspace(str[iOpposite])){
+            hasAnyChar = true;
+            if (!firstCharFoundAtEnd){
+                firstCharFoundAtEnd = true;
+                isQuestion = str[iOpposite] == '?';
+            }
+            if (!std::iscntrl(str[iOpposite]) && !std::isdigit(str[iOpposite]) && !std::ispunct(str[iOpposite])){
+                hasAnyLetters = true;
+                if (!std::isupper(str[iOpposite]))
+                    isAllUpperCase = false;
+            }
+        }
+    }
+
+    isAllUpperCase = hasAnyLetters && isAllUpperCase;
+    
+    if (isQuestion){
         result = isAllUpperCase ? "Calm down, I know what I'm doing!" : "Sure.";
     } else {
-        if (isAllUpperCase) result = "Whoa, chill out!";
+        if (hasAnyChar){
+            if (isAllUpperCase) result = "Whoa, chill out!";
+        } else {
+            result = "Fine. Be that way!";
+        }
     }
 
     return result;
+
 }
 
 }  // namespace bob
--- C++/main.cpp
+++ C++/main.cpp
@@ -5,8 +5,21 @@
 
 int main(){
 
-    auto value = bob::hey("WATCH OUT!");
-
+    auto value = bob::hey("How are you?");
+    std::cout << value << std::endl;
+    value = bob::hey("WATCH OUT!");
+    std::cout << value << std::endl;
+    value = bob::hey("WHAT ARE YOU DOING?");
+    std::cout << value << std::endl;
+    value = bob::hey(" ");
+    std::cout << value << std::endl;
+    value = bob::hey("fasfasfsafasf ");
+    std::cout << value << std::endl;
+    value = bob::hey("1, 2, 3");
+    std::cout << value << std::endl;
+    value = bob::hey("4?");
+    std::cout << value << std::endl;
+    value = bob::hey("\n\r \t");
     std::cout << value << std::endl;
 
     //std::cin.get();
