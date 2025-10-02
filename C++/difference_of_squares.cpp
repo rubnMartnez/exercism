1d70dbd74d2a047bf41f69e2374b97fe18e2006e
rubnMartnez <projects.overstay918@passinbox.com>
2025-10-02 13:32:51 +0200

    second optimization

--- C++/difference_of_squares.cpp
+++ C++/difference_of_squares.cpp
@@ -5,33 +5,16 @@
 namespace difference_of_squares {
 
 // TODO: add your solution here
-uint32_t lastCalculatedNum = 0;
-uint32_t squareOfSum = 0;
-uint32_t sumOfSquares = 0;
-
-void calculate(uint32_t n){
-    if (lastCalculatedNum != n){
-        lastCalculatedNum = n;
-        squareOfSum = 0;
-        sumOfSquares = 0;
-        for (uint32_t i = 1; i <= n; i++){
-            squareOfSum += i;
-            sumOfSquares += std::pow(i, 2);
-        }
-    }
-}
-    
-uint32_t square_of_sum(uint32_t n){
-    calculate(n);
-    return std::pow(squareOfSum, 2);
+uint32_t square_of_sum(uint32_t n) {
+    uint32_t sum = n * (n + 1) / 2;
+    return sum * sum;
 }
 
-uint32_t sum_of_squares(uint32_t n){
-    calculate(n);
-    return sumOfSquares;
+uint32_t sum_of_squares(uint32_t n) {
+    return n * (n + 1) * (2 * n + 1) / 6;
 }
 
-uint32_t difference(uint32_t n){
+uint32_t difference(uint32_t n) {
     return square_of_sum(n) - sum_of_squares(n);
 }
 
