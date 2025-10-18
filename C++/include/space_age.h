587f66a5d160150992d16f476125cb07d37c8f53
rubnMartnez <projects.overstay918@passinbox.com>
2025-10-18 17:22:27 +0200

    add space_age structure

---
+++ C++/include/space_age.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+
+namespace space_age {
+
+// TODO: add your solution here
+
+class space_age
+{
+private:
+    uint64_t mAgeInSeconds{};
+public:
+    explicit space_age(uint64_t age_in_seconds);
+    ~space_age();
+
+    uint64_t seconds() const;
+
+    double on_mercury() const;
+    double on_venus() const;
+    double on_earth() const;
+    double on_mars() const;
+    double on_jupiter() const;
+    double on_saturn() const;
+    double on_uranus() const;
+    double on_neptune() const;
+
+};
+
+
+}  // namespace space_age
+
---
+++ C++/space_age.cpp
@@ -0,0 +1,45 @@
+#include "space_age.h"
+
+namespace space_age {
+
+// TODO: add your solution here
+
+space_age::space_age(uint64_t ageInSeconds) : mAgeInSeconds(ageInSeconds)
+{
+}
+
+space_age::~space_age()
+{
+}
+
+uint64_t space_age::seconds() const{
+    return 0;
+}
+
+double space_age::on_mercury() const{
+    return 0;
+}
+double space_age::on_venus() const{
+    return 0;
+}
+double space_age::on_earth() const{
+    return 0;
+}
+double space_age::on_mars() const{
+    return 0;
+}
+double space_age::on_jupiter() const{
+    return 0;
+}
+double space_age::on_saturn() const{
+    return 0;
+}
+double space_age::on_uranus() const{
+    return 0;
+}
+double space_age::on_neptune() const{
+    return 0;
+}
+
+}  // namespace space_age
+
